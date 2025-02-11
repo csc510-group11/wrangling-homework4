a80f104653c35344ced475e7ae0d3e007ec64b86
NaziaAfreen015 <nazia.afreen015@gmail.com>
2025-02-10 20:53:07 -0500

    Add unit test for task2.sh script output validation

---
+++ tests/test_task2.py
@@ -0,0 +1,37 @@
+import subprocess
+
+def test_task2():
+    # Run the task2.sh script
+    result = subprocess.run(
+        ['/bin/bash' ,'task2.sh'],
+        capture_output=True,
+        text=True
+    )
+
+    # Expected output
+    
+    expected_output ="""Lines containing the word banana in the file sample.txt:
+banana
+banana
+banana
+Lines not containing the word banana in the file sample.txt:
+apple
+apple
+cherry
+date
+Unique lines in the file sample.txt:
+apple
+banana
+cherry
+date
+Count the occurence of each word in the file sample.txt:
+      2 apple
+      3 banana
+      1 cherry
+      1 date
+Count the occurence of the word banana in the file sample.txt:
+      3 banana
+    """
+
+    # Assert the expected output matches the actual output
+    assert result.stdout.strip() == expected_output.strip()
